752009356e4d1667f07dda8bcd69724bd591b764
Drew Taylor <drew.taylor@perficient.com>
2020-06-16 11:29:18 -0400

    Working db connection and sql command

---
+++ module5-5Web/WebForm1.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="module5_5Web.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="height: 319px">
+            <asp:GridView ID="GridView1" runat="server" BackColor="#993300" BorderColor="#993333" Height="275px" Width="349px">
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ module5-5Web/WebForm1.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data.Odbc;
+using System.Data.OleDb;
+//using System.Data.OracleClient;
+
+namespace module5_5Web
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        private string user = "";
+        private string pass = "";
+        private string dataSrc = "data source=PRFT-15123;";
+        private string database = "database=fabrikam2;";
+        private string args = "integrated security=SSPI;";
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            SqlConnection con = new SqlConnection(dataSrc + database + args);
+            SqlCommand cmd = new SqlCommand("SELECT * FROM school", con);
+            con.Open();
+            SqlDataReader rdr = cmd.ExecuteReader();
+            GridView1.DataSource = rdr;
+            GridView1.DataBind();
+            con.Close();
+
+        }
+    }
+}
